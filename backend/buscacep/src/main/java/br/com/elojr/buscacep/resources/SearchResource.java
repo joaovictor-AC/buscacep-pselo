31e83ac4f9625b8c3b73323eb568512be08371a9
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-04-20 15:25:34 -0300

    Adicionado os endpoints referentes a busca

--- backend/buscacep/src/main/java/br/com/elojr/buscacep/resources/SearchResource.java
+++ backend/buscacep/src/main/java/br/com/elojr/buscacep/resources/SearchResource.java
@@ -1,23 +1,33 @@
 package br.com.elojr.buscacep.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.elojr.buscacep.dto.SearchDTO;
+import br.com.elojr.buscacep.dto.SearchInsertDTO;
 import br.com.elojr.buscacep.entities.Search;
 import br.com.elojr.buscacep.entities.User;
 import br.com.elojr.buscacep.services.AuthService;
+import br.com.elojr.buscacep.services.SearchService;
 
 @RestController
 @RequestMapping(value = "/searches")
 public class SearchResource {
+	
+	@Autowired
+	SearchService service;
 	
 	@Autowired
 	AuthService authService;
@@ -30,5 +40,11 @@ public class SearchResource {
 		return list.stream().map(x -> new SearchDTO(x)).collect(Collectors.toList());
 	}
 	
-	//Endpoint de inserir uma busca
+	@PostMapping
+	public ResponseEntity<SearchDTO> insert(@RequestBody SearchInsertDTO dto) {
+		SearchDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
+	}
 }
---
+++ backend/buscacep/src/main/java/br/com/elojr/buscacep/services/SearchService.java
@@ -0,0 +1,42 @@
+package br.com.elojr.buscacep.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.elojr.buscacep.dto.SearchDTO;
+import br.com.elojr.buscacep.dto.SearchInsertDTO;
+import br.com.elojr.buscacep.entities.Search;
+import br.com.elojr.buscacep.entities.User;
+import br.com.elojr.buscacep.repositories.SearchRepository;
+import br.com.elojr.buscacep.repositories.UserRepository;
+
+@Service
+public class SearchService {
+
+	@Autowired
+	SearchRepository repository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	public SearchDTO insert(SearchInsertDTO dto) {
+		Search entity = new Search();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new SearchDTO(entity);
+	}
+
+	private void copyDtoToEntity(SearchInsertDTO dto, Search entity) {
+		entity.setCep(dto.getCep());
+		entity.setBairro(dto.getBairro());
+		entity.setLocalidade(dto.getLocalidade());
+		entity.setCreatedAt(Instant.now());
+		entity.setUf(dto.getUf());
+		entity.setLogradouro(dto.getLogradouro());
+		
+		User user = userRepository.getOne(dto.getUser().getId());
+		entity.setUser(user);
+	}
+}
