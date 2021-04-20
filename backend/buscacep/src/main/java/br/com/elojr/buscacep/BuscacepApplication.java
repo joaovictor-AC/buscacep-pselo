0d49d6eee38643a6e83da3319c273eb33916df0e
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-04-20 14:19:22 -0300

    Commit inicial, backend criado

---
+++ backend/buscacep/src/main/java/br/com/elojr/buscacep/BuscacepApplication.java
@@ -0,0 +1,13 @@
+package br.com.elojr.buscacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscacepApplication.class, args);
+	}
+
+}
