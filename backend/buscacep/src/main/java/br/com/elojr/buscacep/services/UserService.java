2ab44f1084e8b42ec82a0b9fe1a6e4b9dd1bfac7
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-04-20 15:33:13 -0300

    Adicionado checklist do Spring Security

--- backend/buscacep/src/main/java/br/com/elojr/buscacep/services/UserService.java
+++ backend/buscacep/src/main/java/br/com/elojr/buscacep/services/UserService.java
@@ -1,6 +1,11 @@
 package br.com.elojr.buscacep.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,8 +19,10 @@ import br.com.elojr.buscacep.repositories.SearchRepository;
 import br.com.elojr.buscacep.repositories.UserRepository;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private UserRepository repository;
 	
@@ -42,6 +49,17 @@ public class UserService {
 			Search search = searchRepository.getOne(searchDto.getId());
 			entity.getSearches().add(search);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("Usuário não encontrado: " + username);
+			throw new UsernameNotFoundException("Email não encontrado");
+		}
+		logger.info("Usuário encontrado: " + username);
+		return user;
 	}
 
 }
