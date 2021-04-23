e3e7140b725832ca9bef298f32a0b182bf8a62cd
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-04-23 01:03:10 -0300

    Adicionado melhor CSS a pagina home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -4,41 +4,41 @@ import { getSessionData, logout } from "../../core/utils/auth";
 import { makePrivateRequest, makeRequest } from "../../core/utils/request";
 import './style.css';
 
-function Form () {
-    const { register, handleSubmit, setValue } = useForm();
-    const [isCepFound, setIsCepFound] = useState(false);
+function Form() {
+  const { register, handleSubmit, setValue } = useForm();
+  const [isCepFound, setIsCepFound] = useState(false);
 
-    const onSubmit = cepRecebido => {
-        makeRequest({ url: `https://viacep.com.br/ws/${cepRecebido.cep}/json/`, method: 'GET' })
-        .then(response => {
-          setIsCepFound(true);
-          console.log(response);
-          setValue('cepParaBackend', response.data.cep);
-          setValue('logradouro', response.data.logradouro);
-          setValue('bairro', response.data.bairro);
-          setValue('localidade', response.data.localidade);
-          setValue('uf', response.data.uf);
-        })
-    }
+  const onSubmit = cepRecebido => {
+    makeRequest({ url: `https://viacep.com.br/ws/${cepRecebido.cep}/json/`, method: 'GET' })
+      .then(response => {
+        setIsCepFound(true);
+        console.log(response);
+        setValue('cepParaBackend', response.data.cep);
+        setValue('logradouro', response.data.logradouro);
+        setValue('bairro', response.data.bairro);
+        setValue('localidade', response.data.localidade);
+        setValue('uf', response.data.uf);
+      })
+  }
 
-    const onSubmitToBackend = dataToBackend => {
-      const session = getSessionData();
-      const payload = {
-        ...dataToBackend,
-        user: {
-          id: session.userId
-        }
+  const onSubmitToBackend = dataToBackend => {
+    const session = getSessionData();
+    const payload = {
+      ...dataToBackend,
+      user: {
+        id: session.userId
       }
-      makePrivateRequest({ url: 'http://localhost:8080/searches', method: 'POST', data: payload })
     }
+    makePrivateRequest({ url: 'http://localhost:8080/searches', method: 'POST', data: payload })
+  }
 
-    const handleOnClick = () => {
-      logout();
-    }
+  const handleOnClick = () => {
+    logout();
+  }
 
-    return(
-      // <div className="home-container">
-      <>
+  return (
+    <div className="container-geral">
+      <div className="container-above">
         <form onSubmit={handleSubmit(onSubmit)} className="form-above">
           <input
             className="input-search-cep"
@@ -50,67 +50,57 @@ function Form () {
             })}
           />
           <div className="two-buttons">
-            <input type="submit" className="button-search-cep" value="BUSCAR"/>
+            <input type="submit" className="button-search-cep" value="BUSCAR" />
             <button onClick={handleOnClick} className="button-logout">LOGOUT</button>
           </div>
         </form>
-        {isCepFound && 
+      </div>
+
+      {/* {isCepFound && */}
+        <div className="container-below">
           <form onSubmit={handleSubmit(onSubmitToBackend)} className="form-below">
-          <div className="inputs-below">
+            <h1 className="form-below-title">SALVE SUA PESQUISA!</h1>
             <input
               className="input-search-cep-below"
               type="text"
               placeholder="CEP"
               name="cepParaBackend"
-              ref={register({
-                required: true,
-              })}
+              ref={register()}
             />
             <input
               className="input-search-cep-below"
               type="text"
               placeholder="Logradouro"
               name="logradouro"
-              ref={register({
-                required: true,
-              })}
+              ref={register()}
             />
             <input
               className="input-search-cep-below"
               type="text"
               placeholder="Bairro"
               name="bairro"
-              ref={register({
-                required: true,
-              })}
+              ref={register()}
             />
             <input
               className="input-search-cep-below"
               type="text"
               placeholder="Localidade"
               name="localidade"
-              ref={register({
-                required: true,
-              })}
+              ref={register()}
             />
             <input
               className="input-search-cep-below"
               type="text"
               placeholder="UF"
               name="uf"
-              ref={register({
-                required: true,
-              })}
+              ref={register()}
             />
-            <div className="button-belowww">
-              <input type="submit" className="submit-form-below" />
-            </div>
-          </div>
-        </form>
-        }
-        </>
-    // </div>
-    );
+            <button className="button-search-below">ENVIAR</button>
+          </form>
+        </div>
+      {/* } */}
+    </div>
+  );
 }
 
 export default Form;
