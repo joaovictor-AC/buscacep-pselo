a52745fcf67c42e0c981bb6877ac5e287947772a
J0N0S <jvaraujocosta13@gmail.com>
2021-04-23 08:20:11 -0300

    Complementando a estrutura da página de registrar

--- frontend/src/pages/Auth/Register/index.jsx
+++ frontend/src/pages/Auth/Register/index.jsx
@@ -11,29 +11,51 @@ function Register() {
 
   return (
     <AuthCard title="REGISTER">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className="input-registrar"
-          type="text"
-          placeholder="Email"
-          name="email"
-          ref={register({
-            required: true,
-          })}
-        />
+      <div className="container">
+        <div className="content first-content">
+          <div className="first-column">
+            <h2 className="title title-primary">welcome back!</h2>
+            <p className="description description-primary">
+              To keep connected with us
+            </p>
+            <p className="description description-primary">
+              please login with your personal info
+            </p>
+            <button id="signin" className="btn btn-primary">
+              sign in
+            </button>
+          </div>
+          <div className="second-column">
+            <h2 className="title title-second">create account</h2>
+            <p className="description description-second">
+              or use your email for registration:
+            </p>
+            <form onSubmit={handleSubmit(onSubmit)} className="form">
+              <input
+                className="input-registrar"
+                type="text"
+                placeholder="Email"
+                name="email"
+                ref={register({
+                  required: true,
+                })}
+              />
 
-        <input
-          className="input-registrar"
-          type="text"
-          placeholder="Senha"
-          name="password"
-          ref={register({
-            required: true,
-          })}
-        />
+              <input
+                className="input-registrar"
+                type="text"
+                placeholder="Senha"
+                name="password"
+                ref={register({
+                  required: true,
+                })}
+              />
 
-        <input type="submit" className="submit-registrar" />
-      </form>
+              <input type="submit" className="submit-registrar" />
+            </form>
+          </div>
+        </div>
+      </div>
     </AuthCard>
   );
 }
