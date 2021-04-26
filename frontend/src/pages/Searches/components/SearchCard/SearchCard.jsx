ab2612c6f91c6d06ec76dd1448c1b21353c171e1
Brayan Amaral <proffleandro.linux@gmail.com>
2021-04-26 14:58:15 -0300

    Criado o componente SearchCard

---
+++ frontend/src/pages/Searches/components/SearchCard/SearchCard.jsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+function SearchCard (){
+    return(
+        <div className="container-search-card">
+            <h1 className="title-search-card">74605080</h1>
+            <div className="">
+                <p className=""><strong>Localidade:</strong> Goiânia</p>
+                <p className=""><strong>Bairro:</strong> Setor Leste Universitário</p>
+                <p className=""><strong>Logradouro:</strong> Rua 227</p>
+                <p className=""><strong>UF:</strong> GO</p>
+            </div>
+        </div>
+    )
+}
+export default SearchCard;
